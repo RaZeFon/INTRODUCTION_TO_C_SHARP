9ff9342b2572705ab879df8f74cc0b45050a3eb2
Evgenii Golichenkov <neonmaster22@yandex.ru>
2023-09-18 14:52:00 +0300

    add Examples 005 Condition if else

---
+++ Examples/examples_005_Condition_If_Else/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+// функция ToLower() позволяет все символы строки перевести в нижний регистр
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Ура, это Masha!");
+}
+else
+{
+    Console.Write("Привет ");
+    Console.WriteLine(username);
+}
