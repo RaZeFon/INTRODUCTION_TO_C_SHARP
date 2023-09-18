c12ad54044c35cfa40a226f463907bf75fd297ce
Evgenii Golichenkov <neonmaster22@yandex.ru>
2023-09-18 13:48:35 +0300

    add Examples 003 Random

--- Examples/examples_003_Sum/Program.cs
+++ Examples/examples_003_Sum/Program.cs
@@ -6,3 +6,12 @@ int numberA1 = 31;
 int numberB1 = 5;
 int result = numberA1 + numberB1;
 Console.WriteLine(result);
+
+
+// Рандомные значения
+int numberA2 = new Random().Next(1, 10); // 1, 2, 3, ... 9
+Console.WriteLine(numberA2);
+int numberB2 = new Random().Next(1, 10); // 1, 2, 3, ... 9
+Console.WriteLine(numberB2);
+int resultA = numberA2 + numberB2;
+Console.WriteLine(resultA); 
