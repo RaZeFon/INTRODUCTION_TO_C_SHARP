b094efb2bfc6e789687519b7b1023b92dea63a98
Evgenii Golichenkov <neonmaster22@yandex.ru>
2023-09-18 22:47:00 +0300

    Task 04

---
+++ Task/task_04/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Напишите программу, которая на вход принимает трехзначное число и на выходе
+показывает последнюю цифру этого числа. 
+
+пример: 456 -> 6
+        782 -> 2
+        918 -> 8
+*/
+
+
+
+// моё
+using System;
+Console.Clear();
+
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine($"Последняя цифра числа: {number % 10}");
+
+
+// учитель
+using System;
+Console.Clear();
+
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+int x = number % 10;
+Console.WriteLine(x);
+
+
+
+
+
