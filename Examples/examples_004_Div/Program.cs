6b5415416b235ffc4efdf6b54130adbad3cb8c9a
Evgenii Golichenkov <neonmaster22@yandex.ru>
2023-09-18 13:23:25 +0300

    add Examples 004 Div

---
+++ Examples/examples_004_Div/Program.cs
@@ -0,0 +1,14 @@
+﻿int numberA = 10;
+int numberB = 5;
+Console.WriteLine(numberA / numberB);
+
+
+int numberA1 = 12;
+int numberB1 = 5;
+Console.WriteLine(numberA1 / numberB1);
+// Ответом будет 2 потому как работаем с целыми числами
+
+
+double numberA2 = 12;
+double numberB2 = 5;
+Console.WriteLine(numberA2 / numberB2);
