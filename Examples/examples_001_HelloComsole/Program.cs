f6a0a2aeacea8fd7b1c0c63feafc02dfdc3833dd
Evgenii Golichenkov <neonmaster22@yandex.ru>
2023-09-18 08:03:15 +0300

    Examples 001 Hello Console

---
+++ Examples/examples_001_HelloComsole/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("2 Hello World !!!");
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+
+
+
+
+
+
