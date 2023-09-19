d75482fae65aaf964a6c2b6483082ed07e53d512
Evgenii Golichenkov <neonmaster22@yandex.ru>
2023-09-19 08:23:17 +0300

    dz_02 problem

--- HomeWork/dz_02/Program.cs
+++ HomeWork/dz_02/Program.cs
@@ -20,6 +20,7 @@ int number3 = int.Parse(Console.ReadLine());
 
 if ((number1 > number2)&&(number1 > number3)) Console.WriteLine($"Max = {number1}");
 if ((number2 > number1)&&(number2 > number3)) Console.WriteLine($"Max = {number2}");
-else Console.WriteLine($"Max = {number3}");
-
+else {
+        Console.WriteLine($"Max = {number3}");
+}
 
