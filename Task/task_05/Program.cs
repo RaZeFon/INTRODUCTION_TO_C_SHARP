ae314c3dc92dba81eb7bf3ea0b2d4ddcd0a8d807
Evgenii Golichenkov <neonmaster22@yandex.ru>
2023-09-20 22:13:36 +0300

    fix Example 011 add Task 5

---
+++ Task/task_05/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+/*
+Напишите программу, которая выводит случайное число из отрезка [10, 99]
+и показывает наибольшую цифру числа.
+
+например:   78 -> 8
+            12 -> 2
+            85 -> 8
+*/
+
+using System;
+//позволяет использовать функции класса консоль без вызова самого класса
+using static System.Console;
+
+Clear();
+
+int number = new Random().Next(10, 100);
+int a1 = number / 10;
+int a2 = number % 10;
+if (a1 > a2){
+    WriteLine($"{number} -> {a1}");
+}
+else{
+    WriteLine($"{number} -> {a2}");
+}
+
+
+
+
+
