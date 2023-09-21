df89bdcba7d8aeabb1a4413aef170d9b1f8c21f7
Evgenii Golichenkov <neonmaster22@yandex.ru>
2023-09-21 20:12:45 +0300

    add dz_06

---
+++ HomeWork/dz_06/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+что третьей цифры нет.
+
+пример: 645 -> 5
+        78 -> третьей цифры нет
+        32679 -> 6
+*/
+
+using System;
+using static System.Console;
+Clear();
+
+Write("Введите число: ");
+string number = ReadLine();
+int length = 3;
+if (number.Length < length){
+    WriteLine("Третьей цифры нет");
+}
+ThirdNumber(number);
+
+void ThirdNumber(string str_num){
+    for (int i = 0; i < str_num.Length; i++){
+        if (i == 2){
+            WriteLine($"{str_num} -> {str_num[i]}");
+        }
+    }
+}
