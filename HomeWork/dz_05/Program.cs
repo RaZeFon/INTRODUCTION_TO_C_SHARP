c569471db61b2265848b83230b3b1dfb01990cf3
Evgenii Golichenkov <neonmaster22@yandex.ru>
2023-09-21 15:12:11 +0300

    add dz_05

---
+++ HomeWork/dz_05/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Напишите программу, которая принимает на вход трёхзначное число и на выходе 
+показывает вторую цифру этого числа.
+
+пример: 456 -> 5
+        782 -> 8
+        918 -> 1
+*/
+
+using System;
+using static System.Console;
+Clear();
+
+Write("Введите трех значное число: ");
+int threeDigitNumber = int.Parse(ReadLine());
+WriteLine($"Второе число: {SecondNumber(threeDigitNumber)}");
+
+int SecondNumber(int num){
+    int result = (num % 100) / 10;
+    return result;
+}
