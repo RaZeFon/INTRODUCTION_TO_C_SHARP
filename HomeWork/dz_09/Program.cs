82cf9faed6ed4286d21798d59305ded574cd6535
Evgenii Golichenkov <neonmaster22@yandex.ru>
2023-09-23 19:09:42 +0300

    listing 00

---
+++ HomeWork/dz_09/Program.cs
@@ -0,0 +1,48 @@
+﻿
+// НЕ РАБОЧИЙ
+
+/*
+using System;
+
+public class Answer
+{
+    //static bool IsPalindrome(int number){
+    static bool IsPalindrome(int number){
+      // Введите свое решение ниже
+      bool result = true;
+      int size = 5;
+      string length = number.ToString(); 
+      //if ((number < 10000) || (number => 100000)){
+      if (length.Length != size){
+        Console.WriteLine("число не пятизначное");
+        return false;
+      }
+      else{
+        for (int i = 0; i < length.Length; i++){
+            if (number[i] != number[number.Length -1 -i]){
+                
+            }
+            result = false;
+            break;       
+        } 
+        return result;
+      }
+    }
+  
+  // Не удаляйте и не меняйте метод Main! 
+      static public void Main(string[] args) {
+        int number;
+
+        if (args.Length >= 1) {
+            number = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            number = 64546;
+        }
+
+        // Не удаляйте строки ниже
+        bool result = IsPalindrome(number);
+        System.Console.WriteLine($"{result}");
+    }
+}
+*/
