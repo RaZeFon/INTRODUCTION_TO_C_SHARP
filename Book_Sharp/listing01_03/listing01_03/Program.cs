6a8f220e35b69cff49565a8b271d25803886d022
Evgenii Golichenkov <neonmaster22@yandex.ru>
2023-09-24 20:45:30 +0300

    listing 03

---
+++ Book_Sharp/listing01_03/listing01_03/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+Напишите программу, в которой пользователь последовательно водит название 
+текущего дня недели, название месяца и дату (номер дня в месяце). Программа 
+выводит сообщение о сегодняшней дате (день недели, дата, месяц). Используйте 
+консольный ввод и вывод данных. Предложите версию программы, в которой для ввода 
+и вывода данных используются диалоговые окна.
+*/
+
+
+using System;
+using Microsoft.VisualBasic;
+using System.Windows.Forms;
+
+class DateDay
+{
+    static void Main()
+    {
+        string day, date, month;
+        day = Interaction.InputBox("Сегодняшний день? ", "Введите день недели");
+        date = Interaction.InputBox("Сегодняшнее число? ", "Ведите дату");
+        month = Interaction.InputBox("Текущий месяц? ", "Введите месяц");
+        MessageBox.Show($"{day} {date} {month}");
+    }
+}
+
+
+using System;
+
+class DataDay
+{
+    static void Main()
+    {
+        Console.Clear();
+        Console.Title = "Дата";
+        Console.Write("Введите день недели: ");
+        string day = Console.ReadLine();
+        Console.Write("Введите сегодняшнее число:");
+        string date = Console.ReadLine();
+        Console.Write("Введите текущий месяц: ");
+        string month = Console.ReadLine();
+        Console.WriteLine($"{day} {date} {month}");
+        Console.ReadKey();
+    }
+}
