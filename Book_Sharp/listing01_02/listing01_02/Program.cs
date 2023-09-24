36f3e85ee15024ae213eefb32cb0aa6fd9fda27e
Evgenii Golichenkov <neonmaster22@yandex.ru>
2023-09-24 19:46:41 +0300

    listing 02

--- Book_Sharp/listing01_02/listing01_02/Program.cs
+++ Book_Sharp/listing01_02/listing01_02/Program.cs
@@ -1,8 +1,9 @@
 ﻿/*
-Напишите программу, в которой поьзователь вводит имя и возраст. Программа отображает сообшение 
+Напишите программу, в которой пользователь вводит имя и возраст. Программа отображает сообщение 
 об имени и возрасте пользователя. Предложите консольную версию программы и версию, в которой
-данные вводятся и быводятся спомощью диалоговых окон.
+данные вводятся и выводятся с помощью диалоговых окон.
 */
+
 
 using System;
 using Microsoft.VisualBasic;
@@ -10,6 +11,27 @@ using System.Windows.Forms;
 
 class NameAge{
     static void Main(){
+        string name = Interaction.InputBox("Как вас зовут? ", "Введите имя");
+        string age = Interaction.InputBox("Сколько вам лет? ", "Введите возраст");
+        MessageBox.Show("Ты " + name + " и тебе " + age + " лет");
+    }
+}
 
+//===============================================================
+
+using System;
+
+class NameAge
+{
+    static void Main()
+    {
+        Console.Clear();
+        Console.Title  = "Ведите данные о себе: ";
+        Console.Write("Введите имя: ");
+        string name = Console.ReadLine();
+        Console.Write("Введите ваш возраст: ");
+        string age = Console.ReadLine();
+        Console.WriteLine($"Ты {name} и тебе {age} лет");
+        Console.ReadKey();
     }
 }
