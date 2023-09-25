eab4f1ad7b1f266bc419d7b04f51aee2773872d7
Evgenii Golichenkov <neonmaster22@yandex.ru>
2023-09-25 20:11:36 +0300

    listing 09

---
+++ Book_Sharp/listing01_09/listing01_09/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Напишите программу, в которой пользователь водит число, а программой отображается последовательность
+из трех чисел: число на единицу меньше введеного, веденое число и число на единицу больше введеного.
+Предложите версию программы с консольным вводом и выводом данных, а также версию программы, в
+которой ввод и вывод выполняются спомощью диалоговых окон.
+*/
+
+
+using System;
+using Microsoft.VisualBasic;
+using System.Windows.Forms;
+
+class Numbers
+{
+    static void Main()
+    {
+        int number = Int32.Parse(Interaction.InputBox("Введите число: ", "Ввод числа"));
+        MessageBox.Show($"{number - 1}, {number}, {number + 1}");
+    }
+}
+
+
+using System;
+using static System.Console;
+
+class Numbers
+{
+    static void Main()
+    {
+        Clear();
+        Write("Введите число: ");
+        int number = int.Parse(ReadLine());
+        WriteLine($"{number - 1}, {number}, {number + 1}");
+    }
+}
+
