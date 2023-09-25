757e48fc9ed2227fcd99590bf209ae49125719e7
Evgenii Golichenkov <neonmaster22@yandex.ru>
2023-09-25 16:03:29 +0300

    listing 05

---
+++ Book_Sharp/listing01_05/listing01_05/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+Напишите программу, в которой по году рождения определяеться возрвст пользователя.
+Используйте консольный ввод и ввод данных.
+*/
+
+
+using System;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+class CurrentAge
+{
+    static void Main()
+    {
+        int year = 2023;
+        string birth_year = Interaction.InputBox("Введите год своего рождения", "Год рождения");
+        int age = year - Int32.Parse(birth_year);
+        MessageBox.Show($"Вам {age} лет.");
+    }
+}
+
+//==============================================================================
+
+using System;
+using static System.Console;
+
+class CurrentAge
+{
+    static void Main()
+    {
+        Clear();
+        Title = "Ваш возраст";
+        Write("Введите год рождения: ");
+        int year = 2023;
+        int brith_year = int.Parse(ReadLine());
+        WriteLine($"Вам {2023 - brith_year} лет.");
+    }
+}
