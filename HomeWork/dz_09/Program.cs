be2079d273ab68322d742a17e7a474b9eca3bb4c
Evgenii Golichenkov <neonmaster22@yandex.ru>
2023-09-26 12:26:47 +0300

    fix listing 01

--- HomeWork/dz_09/Program.cs
+++ HomeWork/dz_09/Program.cs
@@ -1,32 +1,14 @@
 ﻿
 // НЕ РАБОЧИЙ
 
-/*
+
 using System;
 
 public class Answer
 {
-    //static bool IsPalindrome(int number){
     static bool IsPalindrome(int number){
       // Введите свое решение ниже
-      bool result = true;
-      int size = 5;
-      string length = number.ToString(); 
-      //if ((number < 10000) || (number => 100000)){
-      if (length.Length != size){
-        Console.WriteLine("число не пятизначное");
-        return false;
-      }
-      else{
-        for (int i = 0; i < length.Length; i++){
-            if (number[i] != number[number.Length -1 -i]){
-                
-            }
-            result = false;
-            break;       
-        } 
-        return result;
-      }
+      
     }
   
   // Не удаляйте и не меняйте метод Main! 
@@ -45,4 +27,3 @@ public class Answer
         System.Console.WriteLine($"{result}");
     }
 }
-*/
