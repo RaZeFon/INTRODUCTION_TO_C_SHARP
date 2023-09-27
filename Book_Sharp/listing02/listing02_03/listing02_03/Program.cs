88ba907273b8f254701cff6fe4bd87b69d0506fb
Evgeny Golichenkov <neonmaster22@yandex.ru>
2023-09-27 15:50:58 +0300

    listing 02 03

---
+++ Book_Sharp/listing02/listing02_03/listing02_03/Program.cs
@@ -0,0 +1,46 @@
+﻿/* 
+ Напишите программу, которая проверяет, удовлетворяет ли введеное пользователем число следующим
+ критериям: число делится на 4, и при этом оно не меньше 10.
+*/
+
+
+using System;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+class CheckNumber
+{
+    static void Main()
+    {
+        int number = Int32.Parse(Interaction.InputBox("Проверяем делится ли число на 4\n и при этом оно должно быть не меньше 10", "Проверка"));
+        if (number < 10) MessageBox.Show($"Число {number} меньше 10", "Завершение работы");
+        else {
+            string result_str = (number % 4 == 0) ? "делится на 4" : "неделится на 4";
+            MessageBox.Show($"Число {number} {result_str}");
+        }
+    }
+}
+
+
+
+using System;
+using static System.Console;
+
+class CheckNumber
+{
+    static void Main()
+    {
+        Clear();
+        Title = "Проверка числа";
+        WriteLine("Проверяем что число делится на 4, и при этом оно не меньше 10");
+        Write("Введите число: ");
+        int number = int.Parse(ReadLine());
+        if (number < 10) Write($"{number} меньше 10 ");
+        else
+        {
+            string result_str = (number % 4 == 0) ? "делится на 4 " : "неделится на 4 ";
+            Write($"{number} {result_str}");
+        }
+    }
+}
+
