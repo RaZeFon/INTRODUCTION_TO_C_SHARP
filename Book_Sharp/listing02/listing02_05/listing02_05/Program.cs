855e32324d682dc4e9a8444b0732c20cdb3cef1f
Evgenii Golichenkov <neonmaster22@yandex.ru>
2023-09-27 20:05:57 +0300

    listing02 05

---
+++ Book_Sharp/listing02/listing02_05/listing02_05/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+ Напишите программу, которая проверяет, сколько тысяч во введеном пользователем числе
+(определяется четвертая цифра справа в десятичном представлении числа)
+*/
+
+
+using System;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+class Thousands
+{
+    static void Main()
+    {
+        int number = int.Parse(Interaction.InputBox("Введите число", "Проверка сколько тысяч в числе"));
+        MessageBox.Show($"тысяч = {number / 1000 % 10}", "Результат");
+    }
+}
+
+
+using System;
+using static System.Console;
+
+class Thousands
+{
+    static void Main()
+    {
+        Title = "Cколько тысяч во введеном пользователем числе";
+        Write("Введите число: ");
+        int number = int.Parse(ReadLine());
+        WriteLine($"{number / 1000 % 10} тысяч в числе");
+    }
+}
+
+
