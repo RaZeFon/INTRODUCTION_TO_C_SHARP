8df2e53c88c431d445eb75d3bc348c431f4320e7
Evgenii Golichenkov <neonmaster22@yandex.ru>
2023-09-28 08:28:51 +0300

    listing02 06 половина

---
+++ Book_Sharp/listing02/listing02_06/listing02_06/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ Напишите программу, которая проверяет вторую справа цифру в восьмеричном представлении числа,
+введеного пользователем. Число вводится в десятичном (обычном) представлении.
+*/
+
+
+using System;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+
+class OctalSystem
+{
+    static void Main()
+    {
+        int number = Int32.Parse(Interaction.InputBox("Введите число", "Перевод в восьмеричную систему счисления"));
+        string result_str = string.Empty;
+        while (number > 0)
+        {
+            result_str = Convert.ToString(number % 8) + result_str;
+            number /= 8;
+        }
+        int number2 = (Int32.Parse(result_str) / 10 % 10);
+        MessageBox.Show($"Вторая цифра {number2} востеричного числа {result_str}", "Решение");
+
+    }
+}
