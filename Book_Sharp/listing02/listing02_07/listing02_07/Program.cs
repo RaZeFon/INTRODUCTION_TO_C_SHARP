7c1d4f77cd88db0d49476aec6b6526f330e6e3d6
Evgeny Golichenkov <neonmaster22@yandex.ru>
2023-10-02 17:10:30 +0300

    listing 02 07 Неисправный

---
+++ Book_Sharp/listing02/listing02_07/listing02_07/Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+Напишите программу, которая вычисляет третий бит справа в двоичном представлении числа,
+введеного пользователем. Число вводится в десятичном (обычном) представлении. В программе используется
+оператор побитового сбвига
+*/
+
+/*
+using System;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+class BinarySystem
+{
+    static void Main()
+    {
+        int number = int.Parse(Interaction.InputBox("Введите число ", "Преобразования числа в двоичною систему исчисления"));
+        string result_str = string.Empty;
+        while (number > 0)
+        {
+            result_str = Convert.ToString(number % 2) + result_str;
+            number /= 2;
+        }
+        int number2 = (Convert.ToInt32(result_str) >> 2) % 10;
+        MessageBox.Show($"(10) = {number} -> (2) = {result_str} = 3 бит = {number2}", "третий бит");
+    }
+}
+*/
+
+using System;
+using System.Runtime.InteropServices;
+using static System.Console;
+
+class BinarySystem
+{
+    static void Main()
+    {
+        Title = "третий бит справа в двоичном представлении числа";
+        Write("Введите чиссло: ");
+        int number = int.Parse(ReadLine());
+
+        /*
+        int number = int.Parse(ReadLine());
+        WriteLine(number);
+        string result_str = string.Empty;
+        while (number > 0)
+        {
+            result_str = Convert.ToString(number % 2) + result_str;
+            number /= 2;
+        }
+        int number2 = Convert.ToInt32(result_str);
+        int result = (number2 << 10) % 10;
+        WriteLine($"(2) = {result_str} >> 2 {number2}= 3 бит = {result}", "третий бит");
+        */
+    }
+}
